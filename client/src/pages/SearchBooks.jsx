2836cd6021fe7e1a95c6afcaf1fa94de90cc720d
Drew-Andersen <andrewtandersen@gmail.com>
2024-09-05 15:37:05 -0400

    login issues

--- client/src/pages/SearchBooks.jsx
+++ client/src/pages/SearchBooks.jsx
@@ -74,12 +74,13 @@ const SearchBooks = () => {
       return false;
     }
 
+    console.log('before try')
+    console.log(bookToSave);
     try {
-      const response = await saveBook(bookToSave, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      // const response = await saveBook(bookToSave, token);
+      await saveBook({
+        variables: { bookData: bookToSave }
+      });
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,16 +4,19 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
-      if(context.user){
-        const userData = await User.findOne({ _id: context.user._id }).select('-password');
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+
         return userData;
       }
+
+      throw AuthenticationError;
     },
   },
 
   Mutation: {
-    addUser: async (parent, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
       const token = signToken(user);
 
       return { token, user };
@@ -34,30 +37,33 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-
     saveBook: async (parent, { bookData }, context) => {
-      if(context.user){
+      if (context.user) {
         const updatedUser = await User.findByIdAndUpdate(
-            { _id: context.user._id },
-            { $push: { savedBooks: bookData } },
-            { new: true, runValidators: true }
-        )
+          { _id: context.user._id },
+          { $push: { savedBooks: bookData } },
+          { new: true }
+        );
+
         return updatedUser;
       }
+
       throw AuthenticationError;
     },
     removeBook: async (parent, { bookId }, context) => {
-      if(context.user){
-        const updatedUser = await User.findByIdAndUpdate(
-            { _id: context.user._id },
-            { $push: { savedBooks: bookId } },
-            { new: true, runValidators: true }
-        )
+      if (context.user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id: context.user._id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+
         return updatedUser;
       }
+
       throw AuthenticationError;
-    }
-  }
+    },
+  },
 };
 
 module.exports = resolvers;
